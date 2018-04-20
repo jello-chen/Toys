a4374739dff8481cb4a06beea80f53302875d613
jello chen <jello_chen@live.com>
2018-04-21 00:12:40 +0800

    Init repo

---
+++ Toys/ToysException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Toys
+{
+    public class ToysException : Exception
+    {
+        public ToysException()
+        {
+        }
+
+        public ToysException(string message) : base(message)
+        {
+        }
+
+        public ToysException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ToysException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
